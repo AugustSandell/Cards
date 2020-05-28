263daf25a1d132e4c0aff5dbb82e81c888e500d6
AugustSandell <august.sandell@gmail.com>
2020-05-28 04:40:20 -0700

    update

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, url_for, redirect, send_file
 from app import app
 from game import Deck, Card
 import random
-
+deck = Deck()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -10,9 +10,18 @@ def index():
     title= 'random'
 
     deck = Deck()
+    
+
     deck.shuffle()
 
+
     card = deck.drawCard()
-    #card.show()
+
+    return render_template('index.html', title=title, card=card)
+
+@app.route('/card', methods=['GET', 'POST'])
+def card():
+    title= 'random'
+    card = deck.drawCard()
 
     return render_template('index.html', title=title, card=card)
